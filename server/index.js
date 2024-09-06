187f6e9bdd0ef78a55a7bb2ef0420c83cb06b031
jrw192 <jdrwei@gmail.com>
2024-09-06 16:39:17 -0400

    fix response

--- server/index.js
+++ server/index.js
@@ -161,7 +161,9 @@ app.get('/api/logout/:sid', (req, res) => {
         maxAge: 30 * 24 * 60 * 60 * 1000 // Cookie expires after 30 days
       });
       pool.query('DELETE FROM user_sessions WHERE sid = $1', [sessionId]).then(() => {
-        res.status(200).send({});
+        res.status(200).send({
+          sessionID: '',
+          session: ''});
         res.redirect('/');
       })
         .catch((err) => {
