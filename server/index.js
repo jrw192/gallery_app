334583a2ee7afb3c8db617bb772b1881109d2923
jrw192 <jdrwei@gmail.com>
2024-09-06 14:44:04 -0400

    fix logout cookie

--- server/index.js
+++ server/index.js
@@ -159,7 +159,7 @@ app.get('/api/logout/:sid', (req, res) => {
       res.status(500).send(err);
     }
     else {
-      res.clearCookie('session', res.session.cookie);
+      res.clearCookie('session', req.session.cookie);
       pool.query('DELETE FROM user_sessions WHERE sid = $1', [sessionId]).then(() => {
         console.log('successfully logged out');
         res.redirect('/');
